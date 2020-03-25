2b4f3eb239cf0d894daffdd5edb72b69ad5508ce
koushik <kshanka2@c101.localdomain>
2020-03-25 10:06:25 -0400

    b

--- TFICF/TFICF.java
+++ TFICF/TFICF.java
@@ -141,11 +141,12 @@ public class TFICF {
             while (itr.hasMoreTokens()) {
                 String current_word = itr.nextToken();
                 current_word = current_word.toLowerCase().trim();
+		current_word = current_word.replaceAll("[^a-zA-Z0-9-]", "");
+
                 if (!current_word.isEmpty())
                     {
 			if ( current_word.charAt(0) >= 'a' && current_word.charAt(0) <= 'z' )
 			    {
-				current_word = current_word.replaceAll("[^a-zA-Z0-9-]", "");
 				word.set(current_word+"@"+fileName);
 				context.write(word, one);
 			    }
