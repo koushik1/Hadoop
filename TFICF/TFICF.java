a2d6f4d774e88051bc858833106d521480a36b73
koushik <kshanka2@c38.localdomain>
2020-03-25 22:32:51 -0400

    tet

--- TFICF/TFICF.java
+++ TFICF/TFICF.java
@@ -326,7 +326,7 @@ public class TFICF {
 		    Text out_key = new Text(vals[0]+'@'+key);
 		    int numerator = Integer.parseInt(vals[1]);
 		    int denominator = Integer.parseInt(vals[2]);
-		    double tficfValue = Math.log((double)((numDocs + 1)/(numDocsWithWord + 1)))*Math.log((double)(numerator/denominator) + 1);
+		    double tficfValue = Math.log((double)((numDocs + 1)/(numDocsWithWord + 1)));//*Math.log((double)(numerator/denominator) + 1);
 		    Text out_val = new Text( String.valueOf(tficfValue) );
 		    //Put the output (key,value) pair into the tficfMap instead of doing a context.write
 		    tficfMap.put(out_key,out_val);
